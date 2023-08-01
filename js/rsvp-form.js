ffd4591b60c7b309347093a665f19807b4296866
phuonghanwedding <140967209+phuonghanwedding@users.noreply.github.com>
2023-08-01 11:51:54 +0700

    Update rsvp-form.js

--- js/rsvp-form.js
+++ js/rsvp-form.js
@@ -1,27 +1,27 @@
-// RSVP Form JavaScript
-jQuery(document).ready(function($) {
-    $('.custom-form.rsvp-form').on('submit', function(e) {
-        e.preventDefault();
+// // RSVP Form JavaScript
+// jQuery(document).ready(function($) {
+//     $('.custom-form.rsvp-form').on('submit', function(e) {
+//         e.preventDefault();
 
-        var formData = {
-            name: $('#name').val(),
-            email: $('#email').val(),
-            guests: $('#guests').val(),
-            message: $('#message').val(),
-        };
+//         var formData = {
+//             name: $('#name').val(),
+//             email: $('#email').val(),
+//             guests: $('#guests').val(),
+//             message: $('#message').val(),
+//         };
 
-        $.ajax({
-            method: 'POST',
-            url: 'https://chualonghoa.vn/zif/rsvp/v1/submit', // Replace with the actual URL of your WordPress site
-            data: formData,
-            success: function(response) {
-                // Handle the response here, such as displaying a success message
-                console.log(response);
-            },
-            error: function(error) {
-                // Handle errors here, such as displaying an error message
-                console.error(error);
-            }
-        });
-    });
-});
+//         $.ajax({
+//             method: 'POST',
+//             url: 'https://chualonghoa.vn/zif/rsvp/v1/submit', // Replace with the actual URL of your WordPress site
+//             data: formData,
+//             success: function(response) {
+//                 // Handle the response here, such as displaying a success message
+//                 console.log(response);
+//             },
+//             error: function(error) {
+//                 // Handle errors here, such as displaying an error message
+//                 console.error(error);
+//             }
+//         });
+//     });
+// });
